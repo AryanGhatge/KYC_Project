4f1653e5f0ee23fec3ffaeee975a464b5b3ebeed
Abhijeet <abhijeetdhopare28@gmail.com>
2025-06-08 17:34:30 +0530

    Resolved merge conflicts in index.js and generateSignature.js

--- server/index.js
+++ server/index.js
@@ -13,15 +13,10 @@ const updateUserRoute = require("./routes/updateUser.routes");
 // const profileRoutes = require("./routes/profile.routes");
 const { isAuthenticated } = require("./middleware/auth.middleware");
 const { cloudinaryConnect } = require("./config/cloudinaryConnect");
-
+const livelinessRoutes = require("./routes/validation/liveliness.route");
 
 const panValidationRoutes = require("./routes/validation/panValidation.routes");
-<<<<<<< HEAD
-const fileUpload = require("express-fileupload");
-const livelinessRoutes = require("./routes/validation/liveliness.route");
-=======
 const bankValidationRoutes = require("./routes/validation/bankValidation.routes");
->>>>>>> b310661422a548b4285ec5e26f5fd73ed783a094
 
 const db = require("./config/dbConnect");
 
@@ -69,11 +64,11 @@ app.use(passport.session());
 app.use("/v1/auth", authRoutes);
 app.use("/v1/data", isAuthenticated, updateUserRoute);
 app.use("/v1/validation", panValidationRoutes);
-<<<<<<< HEAD
+
 app.use("/v1/image", livelinessRoutes);
-=======
+
 app.use("/v1/bankValidation", bankValidationRoutes);
->>>>>>> b310661422a548b4285ec5e26f5fd73ed783a094
+
 // app.use("/v1/pan", isAuthenticated, panRoutes);
 // app.use("/v1/address", isAuthenticated, addressRoutes);
 // app.use("/v1/bank", isAuthenticated, bankRoutes);
